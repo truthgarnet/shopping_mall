3db1f1a4610a05958977f1e82748289625304e58
dmstj105 <dmstj105@gmail.com>
2026-09-18 05:58:22 +0900

    chore: 사용하지 않는 import 삭제

--- src/main/java/com/truthgarnet/shopping/order/OrderService.java
+++ src/main/java/com/truthgarnet/shopping/order/OrderService.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
